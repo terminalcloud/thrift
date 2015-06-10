7f00ea50b34f9ee09b4208b30b82359ddefc3a4d
Jonathan Reem <jonathan.reem@gmail.com>
2015-06-10 17:18:06 -0400

    Change Processor::process to receive &self
    This allows a Sync Processor to be used in a multithreaded context.

--- lib/rs/src/processor.rs
+++ lib/rs/src/processor.rs
@@ -3,6 +3,6 @@ use transport::Transport;
 use Result;
 
 pub trait Processor<P: Protocol, T: Transport> {
-    fn process(&mut self, prot: &mut P, transport: &mut T) -> Result<()>;
+    fn process(&self, prot: &mut P, transport: &mut T) -> Result<()>;
 }
 
